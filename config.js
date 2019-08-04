cf60116c5dda6581c6bddad13ba5e09b305d7b1e
Hammy Havoc <hammy@splitanatom.com>
2019-08-04 12:35:55 +0100

    Increased version number

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ define( function ( require ) {
 		wp_ws_url : 'https://www.footballfreetips.com/wp-appkit-api/football-betting-tips-2',
 		wp_url : 'https://www.footballfreetips.com',
 		theme : 'fbtapp',
-		version : '2.6',
+		version : '2.7',
 		app_type : 'phonegap-build',
 		app_title : 'Football Betting Tips',
 		app_platform : 'android',
